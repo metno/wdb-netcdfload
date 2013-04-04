b067d0cc6499f35b109a9dfa327ca93dd19a99bf
vegardb <vegard.bones@met.no>
2013-04-04 15:08:05 +0200

    making distcheck pass

--- doc/netcdfLoad.mk
+++ doc/netcdfLoad.mk
@@ -3,3 +3,6 @@ man1_MANS = netcdfLoad.man
 
 netcdfLoad.man:	netcdfLoad doc/netcdfLoad.txt
 	$(HELP2MAN) -N -n"Load a netcdf file into the WDB database" -i$(top_srcdir)/doc/netcdfLoad.txt ./$< -o$@
+
+EXTRA_DIST += doc/netcdfLoad.txt
+CLEANFILES += netcdfLoad.man
