b19f1024262e8037adcdeee50ddb6a3129387230
vegardb <vegard.bones@met.no>
2013-04-17 14:44:24 +0200

    minor updates

--- etc/netcdfLoad.mk
+++ etc/netcdfLoad.mk
@@ -1,3 +1,4 @@
 dist_pkgsysconf_DATA += etc/netcdfLoad.configuration.xsd \
                            etc/eps2probandltf.netcdfload.xml \
+                           etc/eps2probandltf.eps_yr.netcdfload.xml \
                            etc/arome.netcdfload.xml
