de43060664c97ab67a9d4aeab694cfd425a4de13
Vegard Bønes <vegard.bones@met.no>
2016-08-04 12:54:18 +0200

    adding oceanforecast config

--- etc/netcdfLoad.mk
+++ etc/netcdfLoad.mk
@@ -1,7 +1,8 @@
 dist_pkgsysconf_DATA += etc/netcdfLoad.configuration.xsd \
-						etc/default.netcdfload.xml \
+			etc/default.netcdfload.xml \
                         etc/eps2probandltf.netcdfload.xml \
                         etc/met.eceps.forecasts.netcdfload.xml \
                         etc/arome.netcdfload.xml \
-						etc/vega.netcdfload.xml
-						
+			etc/vega.netcdfload.xml \
+			etc/oceanforecast.xml
+
